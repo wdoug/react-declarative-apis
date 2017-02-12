ba2d560016ec7acfa69c45527971adefa7d742cf
Willy <willygdouglas@gmail.com>
2017-02-11 21:26:11 -0700

    Add React Router 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { get } from './utils/callApi';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+import TodoList from './TodoList';
 
-    this.state = {};
-  }
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/todo">Todo List</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+      </ul>
 
-  componentDidMount() {
-    get('todos').then(todos => {
-      this.setState({
-        todos
-      });
-    });
-  }
+      <hr/>
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <div className="App-intro">
-          { this.state.todos && this.state.todos.map(todo => (
-            <div key={todo.id}>
-              { todo.content }
-            </div>
-          )) }
-        </div>
-      </div>
-    );
-  }
-}
+      <Route exact path="/" component={Home}/>
+      <Route path="/todo" component={TodoList}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+);
+
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li><Link to={`${match.url}/rendering`}>Rendering with React</Link></li>
+      <li><Link to={`${match.url}/components`}>Components</Link></li>
+      <li><Link to={`${match.url}/props-v-state`}>Props v. State</Link></li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
 
 export default App;
+
---
+++ client/src/TodoList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { get } from './utils/callApi';
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    get('todos').then(todos => {
+      this.setState({ todos });
+    });
+  }
+
+  render() {
+    return (
+      <div className="TodoList">
+        { this.state.todos && this.state.todos.map(todo => (
+          <div key={todo.id}>
+            { todo.content }
+          </div>
+        )) }
+      </div>
+    );
+  }
+}
+
+export default TodoList;
