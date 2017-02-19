2b4ec277cb43016cc1d7fbcecc8702683d9a4400
Willy <willygdouglas@gmail.com>
2017-02-19 15:49:17 -0700

    Support following with react-refetch - requires page reload

--- client/src/react-refetch/ProfileContainer.js
+++ client/src/react-refetch/ProfileContainer.js
@@ -1,8 +1,16 @@
 import { connect } from 'react-refetch';
 import Profile from '../presentational/Profile';
 
+const loggedInId = 1;
+
 const ProfileContainer = connect(({ id }) => ({
-  customer: `/api/customers/${id}?filter=${JSON.stringify({ "include": "followers" })}`
+  customer: `/api/customers/${id}?filter=${JSON.stringify({ "include": "followers" })}`,
+  followClicked: id => ({
+    followClickedResponse: {
+      url: `/api/customers/${id}/followers/rel/${loggedInId}`,
+      method: 'PUT'
+    }
+  })
 }))(Profile);
 
 export default ProfileContainer;
